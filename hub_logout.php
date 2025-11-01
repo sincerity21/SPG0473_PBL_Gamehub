a31bb3de31d66e76c7665ce03da4287d4da12552
Fawwaz Shahrom <sincerity2103@gmail.com>
2025-11-01 23:55:08 +0800

    Testing

--- hub_logout.php
+++ hub_logout.php
@@ -2,7 +2,7 @@
 session_start();
 session_destroy();
 
-
+// test
 // // Redirect to login page after session is destroyed
 header("Location: hub_login.php");
 exit(); // Terminate script execution after redirect

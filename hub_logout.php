f15066f763ef482b8e135dd04b9b545f5aff116e
Fawwaz Shahrom <sincerity2103@gmail.com>
2025-11-03 23:37:22 +0800

    Testing Repository.

--- hub_logout.php
+++ hub_logout.php
@@ -3,6 +3,7 @@ session_start();
 session_destroy();
 
 // Testing Public Repository
+// Testing 1 2 3 4 5 
 // // Redirect to login page after session is destroyed
 header("Location: hub_login.php");
 exit(); // Terminate script execution after redirect
